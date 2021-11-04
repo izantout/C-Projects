3f2473b5bcc773011f4b8c1dc4d4f4b710c02aa4
Issam Zantout <80976696+izantout@users.noreply.github.com>
2021-11-04 12:54:08 -0700

    Update main.c

--- LED shuffle using timer/main.c
+++ LED shuffle using timer/main.c
@@ -35,7 +35,7 @@ main(void)
 InitDevice();
 TMOD = 0x01; //Timer 0 is in 16bit Mode
 TL0 = 0x00; // Initializing TH0
-				// when Overflow happens 71ms occure
+															// when Overflow happens 71ms occure
 TH0 = 0x00; // Initializing TL0
 TCON = TCON | (0x01 << 4); //TR0 = 1 starting Timer 0 
 chng = 0x01;
@@ -44,185 +44,92 @@ x=7;
 	
 	while(1) //Infinite while loop, i.e. Superloop
 	{
-		if (~P0 & 0x02) // If button 1 is pushed (button 1 is the added button on the breadboard)
+		for (s=0;s<7;s++)
 		{
-			for (s=0;s<7;s++)
+			if (TCON & (0x01 << 5)) // Check if TF0 = 1 ... If 1 Overflow happened.
 			{
-				if (TCON & (0x01 << 5)) // Check if TF0 = 1 ... If 1 Overflow happened.
-				{
-					TCON = TCON &~(0x01 << 4); // Stopping the timer TR0 = 0
-					TL0 = 0x00;
-					TH0 = 0x00;
-					TCON = TCON &~(0x01 << 5); // Clearing the flag TF0 = 0
-					TCON = TCON | (0x01 << 4); // Start the timer again
-					count = count+1;
-				}
-				if (~P0 & 0x04) // if button 0 is pushed
-				{
-					if (count == 2)
-					{ 
-						chng = chng << 1; //Left shift
-						P1 = chng;
-						count = 0; //reset count
-						x = x-1;
-					}
-					else
-					{
-						chng = chng;
-						P1 = chng;
-					}
+				TCON = TCON &~(0x01 << 4); // Stopping the timer TR0 = 0
+				TL0 = 0x00;
+				TH0 = 0x00;
+				TCON = TCON &~(0x01 << 5); // Clearing the flag TF0 = 0
+				TCON = TCON | (0x01 << 4); // Start the timer again
+				count = count+1;
+			}
+			if (~P0 & 0x04) // if button 0 is pushed
+			{
+				if (count == 2)
+				{ 
+					chng = chng << 1; //Left shift
+					P1 = ~chng;
+					count = 0; //reset count
+					x = x-1;
 				}
 				else
 				{
-					if (count == 5) // Button is not pushed
-					{
-						chng = chng << 1; //Left shift
-						P1 = chng;
-						count = 0; //reset count
-						x = x-1;
-					}
-					else
-					{
-						chng = chng;
-						P1 = chng;
-					}
-				}	
-				if (x < 1 & x > -1)
-				{
-					for (s=0;x<7;s++)
-					{
-						if (TCON & (0x01 << 5)) // Check if TF0 = 1 ... If 1 Overflow happened.
-						{
-							TCON = TCON &~(0x01 << 4); // Stopping the timer TR0 = 0
-							TL0 = 0x00;
-							TH0 = 0x00;
-							TCON = TCON &~(0x01 << 5); // Clearing the flag TF0 = 0
-							TCON = TCON | (0x01 << 4); // Start the timer again
-							count = count+1;
-						}
-						if (~P0 & 0x04) // if button is pushed
-						{
-							if (count == 2)
-							{ 
-								chng = chng >> 1; //Right shift
-								P1 = chng;
-								count = 0; //reset count
-								x = x+1;
-							}
-							else
-							{
-								chng = chng;
-								P1 = chng;
-							}
-						}
-						else
-						{
-							if (count == 5) // Button is not pushed
-							{
-								chng = chng >> 1; //Right shift
-								P1 = chng;
-								count = 0; //reset count
-								x = x+1;
-							}
-							else
-							{
-								chng = chng;
-								P1 = chng;
-							}
-						}
-					}	
-				}	
-			}	
-		}
-		else
-		{
-			for (s=0;s<7;s++)
-			{
-				if (TCON & (0x01 << 5)) // Check if TF0 = 1 ... If 1 Overflow happened.
-				{
-					TCON = TCON &~(0x01 << 4); // Stopping the timer TR0 = 0
-					TL0 = 0x00;
-					TH0 = 0x00;
-					TCON = TCON &~(0x01 << 5); // Clearing the flag TF0 = 0
-					TCON = TCON | (0x01 << 4); // Start the timer again
-					count = count+1;
+					chng = chng;
+					P1 = ~chng;
 				}
-				if (~P0 & 0x04) // if button 0 is pushed
+			}
+			else
+			{
+				if (count == 5) // Button is not pushed
 				{
-					if (count == 2)
-					{ 
-						chng = chng << 1; //Left shift
-						P1 = ~chng;
-						count = 0; //reset count
-						x = x-1;
-					}
-					else
-					{
-						chng = chng;
-						P1 = ~chng;
-					}
+					chng = chng << 1; //Left shift
+					P1 = ~chng;
+					count = 0; //reset count
+					x = x-1;
 				}
 				else
 				{
-					if (count == 5) // Button is not pushed
-					{
-						chng = chng << 1; //Left shift
-						P1 = ~chng;
-						count = 0; //reset count
-						x = x-1;
-					}
-					else
-					{
-						chng = chng;
-						P1 = ~chng;
-					}
-				}	
-				if (x < 1 & x > -1)
+					chng = chng;
+					P1 = ~chng;
+				}
+			}	
+			if (x < 1 & x > -1)
+			{
+				for (s=0;x<7;s++)
 				{
-					for (s=0;x<7;s++)
+					if (TCON & (0x01 << 5)) // Check if TF0 = 1 ... If 1 Overflow happened.
 					{
-						if (TCON & (0x01 << 5)) // Check if TF0 = 1 ... If 1 Overflow happened.
-						{
-							TCON = TCON &~(0x01 << 4); // Stopping the timer TR0 = 0
-							TL0 = 0x00;
-							TH0 = 0x00;
-							TCON = TCON &~(0x01 << 5); // Clearing the flag TF0 = 0
-							TCON = TCON | (0x01 << 4); // Start the timer again
-							count = count+1;
-						}
-						if (~P0 & 0x04) // if button is pushed
-						{
-							if (count == 2)
-							{ 
-								chng = chng >> 1; //Right shift
-								P1 = ~chng;
-								count = 0; //reset count
-								x = x+1;
-							}
-							else
-							{
-								chng = chng;
-								P1 = ~chng;
-							}
+						TCON = TCON &~(0x01 << 4); // Stopping the timer TR0 = 0
+						TL0 = 0x00;
+						TH0 = 0x00;
+						TCON = TCON &~(0x01 << 5); // Clearing the flag TF0 = 0
+						TCON = TCON | (0x01 << 4); // Start the timer again
+						count = count+1;
+					}
+					if (~P0 & 0x04) // if button is pushed
+					{
+						if (count == 2)
+						{ 
+							chng = chng >> 1; //Right shift
+							P1 = ~chng;
+							count = 0; //reset count
+							x = x+1;
 						}
 						else
 						{
-							if (count == 5) // Button is not pushed
-							{
-								chng = chng >> 1; //Right shift
-								P1 = ~chng;
-								count = 0; //reset count
-								x = x+1;
-							}
-							else
-							{
-								chng = chng;
-								P1 = ~chng;
-							}
+							chng = chng;
+							P1 = ~chng;
 						}
-					}	
+					}
+					else
+					{
+						if (count == 5) // Button is not pushed
+						{
+							chng = chng >> 1; //Right shift
+							P1 = ~chng;
+							count = 0; //reset count
+							x = x+1;
+						}
+						else
+						{
+							chng = chng;
+							P1 = ~chng;
+						}
+					}
 				}	
 			}	
-		}
+		}	
 	}
 }
