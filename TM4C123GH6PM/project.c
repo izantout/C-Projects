88a645dd8e5d11881d0d4fca20011e50e77e3fa6
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-03-12 19:24:49 -0800

    Update project.c

--- TM4C123GH6PM/project.c
+++ TM4C123GH6PM/project.c
@@ -36,9 +36,16 @@ void portInit(void)
 	GPIO_PORTE_DIR_R = 0x3F;  // 5) PE2,PE1,PE0 output
 	GPIO_PORTE_DEN_R = 0x7F;  // 8) enable digital pins PE5-PE0
 // PORT F		
-		SYSCTL_RCGC2_R |= 0X0000020; // PF clock
-	GPIO_PORTF_DIR_R = 	0x0A; // 5) PF4 input, PF3,PF1 output
-	GPIO_PORTF_DEN_R =  0x0C; // 8) enable digital pins PF4-PF0
+  SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
+  delay = SYSCTL_RCGC2_R;           // delay   
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
+  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0       
+  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
+  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
+  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
+  GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
+  GPIO_PORTF_PUR_R = 0x11;          // enable pullup resistors on PF4,PF0       
+  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0 
 	}
 
     
